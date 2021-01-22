beb71465d08c7611163b21e58fa3b49fac6657c8
Ka Chun <34387770+kachun333@users.noreply.github.com>
2021-01-22 22:54:07 +0800

    Create meatViewAll.dart

---
+++ lib/view/meat/meatViewAll.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:foody_app/view/meat/meatCreate.dart';
+import 'package:foody_app/widget/app_bar.dart';
+
+class MeatViewAll extends StatefulWidget {
+  MeatViewAll({Key key}) : super(key: key);
+
+  @override
+  _MeatViewAllState createState() => _MeatViewAllState();
+}
+
+class _MeatViewAllState extends State<MeatViewAll> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: FoodyAppBar(),
+      backgroundColor: Colors.greenAccent,
+      body: Center(
+        child: IconButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => MeatCreate()),
+            );
+          },
+          icon: Icon(Icons.alternate_email),
+          color: Colors.amber,
+        ),
+      ),
+    );
+  }
+}
