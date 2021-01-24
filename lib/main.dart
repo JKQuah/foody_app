7a6d75284cec9eb7088f0545ba3b685df3f99252
JKQuah <kobekai1997@gmail.com>
2021-01-24 17:07:32 +0800

    Hard code login jwtoken

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:foody_app/utils/sharedPreferenceUtils.dart';
 import 'package:foody_app/widget/bottom_nav_bar.dart';
 
 void main() {
@@ -26,6 +27,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    String jwtToken =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjIiLCJpYXQiOjE2MTEzOTQyMTcsImV4cCI6MTYxMTQ4MDYxN30.E2EGbAfh9tIOhYm3YHneq_4Ii5hml_SCNBQx7dLq7lk";
+    SharedPreferenceUtils.saveJWToken(jwtToken);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return BottomNavBar();
