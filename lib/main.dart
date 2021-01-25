356fd913ee9047580f3c476603f9aaf67d0700e2
JKQuah <kobekai1997@gmail.com>
2021-01-25 22:55:08 +0800

    Update splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:foody_app/utils/sharedPreferenceUtils.dart';
-import 'package:foody_app/widget/bottom_nav_bar.dart';
+import 'package:foody_app/view/loadingView.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,33 +9,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // initialRoute: '/',
+      // routes: {
+      //   '/': (context) => Loading(),
+      //   '/home': (context) => MyApp(),
+      // },
       theme: ThemeData(
         primaryColor: Colors.white,
         fontFamily: 'Nexa',
       ),
-      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+      home: Loading(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  void initState() {
-    String jwtToken =
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjIiLCJpYXQiOjE2MTE0ODEzNTcsImV4cCI6MTYxMTU2Nzc1N30.1SrS_GEacUG-BTI2SxcUuvN6vOT9KsA0N4OJ657x3FI";
-    SharedPreferenceUtils.saveJWToken(jwtToken);
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return BottomNavBar();
   }
 }
---
+++ lib/view/loadingView.dart
@@ -0,0 +1,67 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:foody_app/resource/app_colors.dart';
+import 'indexView.dart';
+
+class Loading extends StatefulWidget {
+  Loading({Key key}) : super(key: key);
+
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  void setupHomeView() async {
+    Timer(
+        Duration(seconds: 3),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => MyHomePage())));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupHomeView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Flexible(
+            flex: 10,
+            child: Center(
+              child: Image(
+                image: AssetImage("assets/app_logo.png"),
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Text(
+              "Among Us 2.0",
+              style: TextStyle(
+                color: AppColors.PRIMARY_COLOR,
+                fontWeight: FontWeight.bold,
+                fontSize: 24.0,
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Text(
+              "WIX3004 Mobile Application Development",
+              style: TextStyle(
+                color: AppColors.ACCENT_COLOR,
+                fontSize: 16.0,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
